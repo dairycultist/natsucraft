95ccb369b5b75f2652a1e5aeef11bf0f6df3adb6
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2025-01-14 00:45:41 -0500

    set up mod

---
+++ src/main/java/net/bluebunnex/template/Template.java
@@ -0,0 +1,21 @@
+package net.bluebunnex.template;
+
+import net.mine_diver.unsafeevents.listener.EventListener;
+import net.modificationstation.stationapi.api.event.registry.ItemRegistryEvent;
+import net.modificationstation.stationapi.api.mod.entrypoint.Entrypoint;
+import net.modificationstation.stationapi.api.util.Namespace;
+import net.modificationstation.stationapi.api.util.Null;
+
+public class Template {
+
+    @Entrypoint.Namespace
+    public static final Namespace NAMESPACE = Null.get();
+
+    @EventListener
+    public void registerItems(ItemRegistryEvent event) {
+
+        for (int i=0; i<10; i++) {
+            System.out.println(NAMESPACE.getName() + " is registering items!!!!");
+        }
+    }
+}
