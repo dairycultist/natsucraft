99751cbe4f9efa10cc8c86eaddc0fce432aca29e
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2025-01-16 14:57:12 -0500

    add iron bauble, remove tomat

--- README.md
+++ README.md
@@ -1,13 +1,3 @@
 > [!IMPORTANT]
-> I'm adding whatever I want to this mod, on the condition that it enhances my survival experience. This means doing runs in survival and deducing from that what to add.
-
-notes
-
-sidegrade... not upgrade!
-
-I don't like how in the first 5 minutes of the game, you can immediately jump right from wood to stone to iron tools
-without even going underground. Should make iron no longer spawn at the surface at least!!
-
-I like food.
-- Added tomato
-- Added curry
+> I'm adding whatever I want to this mod, on the condition that it enhances my survival experience. This means doing
+> runs in survival and deducing from that what to add.
--- src/main/java/net/bluebunnex/template/Template.java
+++ src/main/java/net/bluebunnex/template/Template.java
@@ -1,13 +1,10 @@
 package net.bluebunnex.template;
 
 import net.mine_diver.unsafeevents.listener.EventListener;
-import net.minecraft.block.Block;
 import net.minecraft.item.Item;
-import net.modificationstation.stationapi.api.event.registry.BlockRegistryEvent;
 import net.modificationstation.stationapi.api.event.registry.ItemRegistryEvent;
 import net.modificationstation.stationapi.api.mod.entrypoint.Entrypoint;
-import net.modificationstation.stationapi.api.template.item.TemplateFoodItem;
-import net.modificationstation.stationapi.api.template.item.TemplateMushroomStewItem;
+import net.modificationstation.stationapi.api.template.item.TemplateItem;
 import net.modificationstation.stationapi.api.util.Namespace;
 import net.modificationstation.stationapi.api.util.Null;
 
@@ -16,26 +13,13 @@ public class Template {
     @Entrypoint.Namespace
     public static final Namespace NAMESPACE = Null.get();
 
-    public static Item TOMATO;
-    public static Item CURRY;
-
-    public static Block TOMATO_CROP;
+    public static Item IRON_BAUBLE;
 
     @EventListener
     public void registerItems(ItemRegistryEvent event) {
 
-        TOMATO = new TemplateFoodItem(NAMESPACE.id("tomato"), 2, false)
-                .setMaxCount(8)
-                .setTranslationKey(NAMESPACE, "tomato");
-
-        CURRY = new TemplateMushroomStewItem(NAMESPACE.id("curry"), 10)
-                .setTranslationKey(NAMESPACE, "curry");
-    }
-
-    @EventListener
-    public void registerBlocks(BlockRegistryEvent event) {
-
-        TOMATO_CROP = new TomatoCropBlock(NAMESPACE.id("tomato_crop"))
-                .setTranslationKey(NAMESPACE, "tomato_crop");
+        IRON_BAUBLE = new TemplateItem(NAMESPACE.id("iron_bauble"))
+                .setMaxCount(1)
+                .setTranslationKey(NAMESPACE, "iron_bauble");
     }
 }
