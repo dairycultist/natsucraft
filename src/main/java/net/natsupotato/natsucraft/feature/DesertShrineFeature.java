368bdd44abfd60ce8a87c10fc3ea676ed01f635d
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2025-05-18 21:14:44 -0400

    Add DesertShrineFeature room population

--- src/main/java/net/natsupotato/natsucraft/feature/DesertShrineFeature.java
+++ src/main/java/net/natsupotato/natsucraft/feature/DesertShrineFeature.java
@@ -1,6 +1,10 @@
 package net.natsupotato.natsucraft.feature;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.entity.ChestBlockEntity;
+import net.minecraft.block.entity.MobSpawnerBlockEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
 import net.minecraft.world.gen.feature.Feature;
 import net.natsupotato.natsucraft.Natsucraft;
@@ -16,26 +20,14 @@ public class DesertShrineFeature extends Feature {
             return false;
 
         // base/foundation
-        for (int ox = x - 3; ox <= x + 3; ox++) {
-            for (int oz = z - 3; oz <= z + 3; oz++) {
+        GenerationHelper.fillRect(world, Block.SANDSTONE.id, x - 3, y - 5, z - 3, 7, 5, 7);
 
-                world.setBlockWithoutNotifyingNeighbors(ox, y, oz, Block.SLAB.id, 1);
-
-                world.setBlockWithoutNotifyingNeighbors(ox, y - 1, oz, Block.SANDSTONE.id);
-                world.setBlockWithoutNotifyingNeighbors(ox, y - 2, oz, Block.SANDSTONE.id);
-                world.setBlockWithoutNotifyingNeighbors(ox, y - 3, oz, Block.SANDSTONE.id);
-                world.setBlockWithoutNotifyingNeighbors(ox, y - 4, oz, Block.SANDSTONE.id);
-                world.setBlockWithoutNotifyingNeighbors(ox, y - 5, oz, Block.SANDSTONE.id);
-            }
-        }
+        for (int ox = x - 3; ox <= x + 3; ox++)
+            for (int oz = z - 3; oz <= z + 3; oz++)
+                world.setBlockWithoutNotifyingNeighbors(ox, y, oz, Block.SLAB.id, 1); // should probably add a helper with meta
 
         // center platform
-        for (int ox = x - 2; ox <= x + 2; ox++) {
-            for (int oz = z - 2; oz <= z + 2; oz++) {
-
-                world.setBlockWithoutNotifyingNeighbors(ox, y, oz, Block.SANDSTONE.id);
-            }
-        }
+        GenerationHelper.fillRect(world, Block.SANDSTONE.id, x - 2, y, z - 2, 5, 1, 5);
 
         // maze
         GenerationHelper.generateMaze(
@@ -46,8 +38,67 @@ public class DesertShrineFeature extends Feature {
                 Block.SANDSTONE.id,
                 (localWorld, localRandom, localX, localY, localZ) -> {
 
-                    if (localRandom.nextBoolean())
-                        GenerationHelper.replaceRect(world, Block.SANDSTONE.id, Natsucraft.LAPIS_SANDSTONE.id, localX, localY + 1, localZ, 9, 1, 9);
+                    int roomType = localRandom.nextInt(6);
+
+                    if (roomType == 0 || roomType == 1) {
+                        localWorld.setBlock(localX + 1, localY + 4, localZ + 4, Block.TORCH.id);
+                        localWorld.setBlock(localX + 7, localY + 4, localZ + 4, Block.TORCH.id);
+                        localWorld.setBlock(localX + 4, localY + 4, localZ + 1, Block.TORCH.id);
+                        localWorld.setBlock(localX + 4, localY + 4, localZ + 7, Block.TORCH.id);
+                    }
+
+                    switch (roomType) {
+
+                        // 1/3 chance of spawner room
+                        case 0:
+                        case 1:
+
+                            // either spiders or mummies (zombies as placeholder)
+                            String monster;
+
+                            if (localRandom.nextBoolean()) {
+                                monster = "Zombie";
+                            } else {
+                                monster = "Spider";
+
+                                for (int ox = localX + 1; ox < localX + 8; ox++)
+                                    for (int oz = localZ + 1; oz < localZ + 8; oz++)
+                                        for (int oy = localY + 1; oy < localY + 5; oy++)
+                                            if (localWorld.getBlockId(ox, oy, oz) == 0 && localRandom.nextInt(3) == 0)
+                                                localWorld.setBlock(ox, oy, oz, Block.COBWEB.id);
+                            }
+
+                            world.setBlock(localX + 4, localY + 1, localZ + 4, Block.SPAWNER.id);
+                            ((MobSpawnerBlockEntity) world.getBlockEntity(localX + 4, localY + 1, localZ + 4))
+                                    .setSpawnedEntityId(monster);
+
+                            break;
+
+                        // 1/6 chance of treasure room
+                        case 2:
+                            localWorld.setBlock(localX + 4, localY + 2, localZ + 4, Block.CHEST.id);
+                            ChestBlockEntity chest = (ChestBlockEntity) localWorld.getBlockEntity(localX + 4, localY + 2, localZ + 4);
+
+                            chest.setStack(localRandom.nextInt(chest.size()), new ItemStack(Item.GOLD_INGOT, localRandom.nextInt(2, 5)));
+                            chest.setStack(localRandom.nextInt(chest.size()), new ItemStack(Item.GOLD_INGOT, localRandom.nextInt(2, 5)));
+                            chest.setStack(localRandom.nextInt(chest.size()), new ItemStack(Item.IRON_INGOT, localRandom.nextInt(2, 5)));
+
+                            if (localRandom.nextBoolean())
+                                chest.setStack(localRandom.nextInt(chest.size()), new ItemStack(Item.GOLDEN_APPLE, 1));
+
+                            GenerationHelper.fillRect(localWorld, Block.SANDSTONE.id, localX + 3, localY + 1, localZ + 3, 3, 1, 3);
+                            GenerationHelper.replaceRect(localWorld, Block.SANDSTONE.id, Natsucraft.LAPIS_SANDSTONE.id, localX, localY + 1, localZ, 9, 1, 9);
+                            break;
+
+                        // 1/6 chance of weird pillar room
+                        case 3:
+                            GenerationHelper.fillRect(localWorld, Block.SANDSTONE.id, localX + 3, localY + 1, localZ + 3, 3, 4, 3);
+                            break;
+
+                        // 1/3 chance of hallway room
+                        default:
+                            break;
+                    }
                 }
         );
 
