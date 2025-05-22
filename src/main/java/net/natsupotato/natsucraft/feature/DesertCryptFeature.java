59f319380bbc395f47cfba46d8a540ed2aef348a
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2025-05-22 18:34:47 -0400

    Update crypt room frequencies

--- src/main/java/net/natsupotato/natsucraft/feature/DesertCryptFeature.java
+++ src/main/java/net/natsupotato/natsucraft/feature/DesertCryptFeature.java
@@ -38,9 +38,9 @@ public class DesertCryptFeature extends Feature {
                 Block.SANDSTONE.id,
                 (localWorld, localRandom, localX, localY, localZ) -> {
 
-                    int roomType = localRandom.nextInt(6);
+                    int roomType = localRandom.nextInt(10);
 
-                    if (roomType != 0 && roomType != 1) {
+                    if (roomType == 0 || roomType == 1 || roomType == 2) {
                         localWorld.setBlock(localX + 1, localY + 4, localZ + 4, Block.TORCH.id);
                         localWorld.setBlock(localX + 7, localY + 4, localZ + 4, Block.TORCH.id);
                         localWorld.setBlock(localX + 4, localY + 4, localZ + 1, Block.TORCH.id);
@@ -51,53 +51,8 @@ public class DesertCryptFeature extends Feature {
 
                     switch (roomType) {
 
-                        // spawner room + chest room
-                        case 0:
-                        case 1:
-
-                            // either spiders or mummies (zombies as placeholder)
-                            String monster;
-
-                            if (localRandom.nextBoolean()) {
-                                monster = "Mummy";
-                            } else {
-                                monster = "Spider";
-
-                                for (int ox = localX + 1; ox < localX + 8; ox++)
-                                    for (int oz = localZ + 1; oz < localZ + 8; oz++)
-                                        for (int oy = localY + 1; oy < localY + 5; oy++)
-                                            if (localWorld.getBlockId(ox, oy, oz) == 0 && localRandom.nextInt(3) == 0)
-                                                localWorld.setBlock(ox, oy, oz, Block.COBWEB.id);
-                            }
-
-                            localWorld.setBlock(localX + 4, localY + 1, localZ + 4, Block.SPAWNER.id);
-                            ((MobSpawnerBlockEntity) localWorld.getBlockEntity(localX + 4, localY + 1, localZ + 4))
-                                    .setSpawnedEntityId(monster);
-
-                        // just chest room
-                        case 2:
-
-                            if (localRandom.nextBoolean())
-                                chest = GenerationHelper.lootChest(localWorld, localRandom, localX + 1, localY + 1, localZ + 2, 4,
-                                        new Item[]{Natsucraft.GLOWSAP, Natsucraft.FABRIC, Item.IRON_INGOT},
-                                        new int[]{2, 2, 1},
-                                        new int[]{6, 5, 4}
-                                );
-
-                            if (localRandom.nextBoolean())
-                                chest = GenerationHelper.lootChest(localWorld, localRandom, localX + 7, localY + 1, localZ + 6, 4,
-                                        new Item[]{Natsucraft.GLOWSAP, Natsucraft.FABRIC, Item.IRON_INGOT},
-                                        new int[]{2, 2, 1},
-                                        new int[]{6, 5, 4}
-                                );
-
-                            if (chest != null && localRandom.nextInt(20) == 0)
-                                chest.setStack(localRandom.nextInt(chest.size()), new ItemStack(Item.CHAIN_CHESTPLATE));
-
-                            break;
-
                         // treasure room
-                        case 3:
+                        case 0:
                             chest = GenerationHelper.lootChest(localWorld, localRandom, localX + 4, localY + 2, localZ + 4, 4,
                                     new Item[] { Item.GOLD_INGOT, Item.IRON_INGOT, Item.DIAMOND },
                                     new int[] { 2, 2, 1 },
@@ -112,12 +67,12 @@ public class DesertCryptFeature extends Feature {
                             break;
 
                         // weird pillar room
-                        case 4:
+                        case 1:
                             GenerationHelper.fillRect(localWorld, Block.SANDSTONE.id, localX + 3, localY + 1, localZ + 3, 3, 4, 3);
                             break;
 
                         // tomb
-                        case 5:
+                        case 2:
                             GenerationHelper.fillRect(localWorld, Block.SANDSTONE.id, localX + 2, localY + 1, localZ + 3, 5, 1, 3);
                             localWorld.setBlockWithoutNotifyingNeighbors(localX + 2, localY + 1, localZ + 3, Block.SLAB.id, 1);
                             localWorld.setBlockWithoutNotifyingNeighbors(localX + 2, localY + 1, localZ + 5, Block.SLAB.id, 1);
@@ -137,6 +92,51 @@ public class DesertCryptFeature extends Feature {
 
                             if (localRandom.nextBoolean())
                                 chest.setStack(localRandom.nextInt(chest.size()), new ItemStack(Item.IRON_SWORD, 1));
+
+                        // spawner room/chest room
+                        default:
+
+                            // either spiders or mummies or none
+                            String monster = null;
+
+                            if (localRandom.nextBoolean()) {
+                                monster = "Mummy";
+                            } else if (localRandom.nextBoolean()) {
+                                monster = "Spider";
+
+                                for (int ox = localX + 1; ox < localX + 8; ox++)
+                                    for (int oz = localZ + 1; oz < localZ + 8; oz++)
+                                        for (int oy = localY + 1; oy < localY + 5; oy++)
+                                            if (localWorld.getBlockId(ox, oy, oz) == 0 && localRandom.nextInt(3) == 0)
+                                                localWorld.setBlock(ox, oy, oz, Block.COBWEB.id);
+                            }
+
+                            if (monster != null) {
+
+                                localWorld.setBlock(localX + 4, localY + 1, localZ + 4, Block.SPAWNER.id);
+                                ((MobSpawnerBlockEntity) localWorld.getBlockEntity(localX + 4, localY + 1, localZ + 4))
+                                        .setSpawnedEntityId(monster);
+                            }
+
+                            // chests
+                            if (localRandom.nextBoolean())
+                                chest = GenerationHelper.lootChest(localWorld, localRandom, localX + 1, localY + 1, localZ + 2, 4,
+                                        new Item[]{Natsucraft.GLOWSAP, Natsucraft.FABRIC, Item.IRON_INGOT},
+                                        new int[]{2, 2, 1},
+                                        new int[]{6, 5, 4}
+                                );
+
+                            if (localRandom.nextBoolean())
+                                chest = GenerationHelper.lootChest(localWorld, localRandom, localX + 7, localY + 1, localZ + 6, 4,
+                                        new Item[]{Natsucraft.GLOWSAP, Natsucraft.FABRIC, Item.IRON_INGOT},
+                                        new int[]{2, 2, 1},
+                                        new int[]{6, 5, 4}
+                                );
+
+                            if (chest != null && localRandom.nextInt(20) == 0)
+                                chest.setStack(localRandom.nextInt(chest.size()), new ItemStack(Item.CHAIN_CHESTPLATE));
+
+                            break;
                     }
                 }
         );
